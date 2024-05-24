14211a05971f19cd1d4d6c7f48a293bc7f5fcd14
Rokku <quocloctran357@gmail.com>
2024-05-24 20:57:32 +0700

    Initial Commit

---
+++ RokGame/RokGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RokGameApp.swift
+//  RokGame
+//
+//  Created by Quốc Lộc on 24/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct RokGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
